b821f36bb7e2b88e766ed62ba930cbd9c046c485
ilanHascal1237 <ilanhascal1237@gmail.com>
2020-11-20 20:47:14 +0000

    able to spawn a test cylinder into an empty world...had to use a github repo to convert sdf to urdf but was able to run a simple serviceProvy to load the model

---
+++ src/spawn.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import rospy, tf
+from gazebo_msgs.srv import DeleteModel, SpawnModel
+from geometry_msgs.msg import *
+
+if __name__ == '__main__':
+    print("Waiting for gazebo services...")
+    spawn_model_client = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
+    spawn_model_client(
+    model_name='TESTCYLINDER',
+    model_xml=open('/my_ros_data/catkin_ws/src/ros-autonomous-pacman/models/TESTCYLINDER/TESTCYLINDER.urdf', 'r').read(), 
+    initial_pose=Pose(),
+    reference_frame='world'
+    )
