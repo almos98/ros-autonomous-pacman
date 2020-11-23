b621d3b3dc94b06f465c869d99c664030a90db4a
ilanHascal1237 <ilanhascal1237@gmail.com>
2020-11-23 21:24:28 +0000

    removed comma from delete.py still needs to be tested

--- src/delete.py
+++ src/delete.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     print("Waiting for gazebo services...")
     spawn_model_client = rospy.ServiceProxy('/gazebo/spawn_sdf_model', DeleteModel)
     spawn_model_client(
-    model_name='TESTCYLINDER',
+    model_name='TESTCYLINDER'
     )
     print("deleted")
