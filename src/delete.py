fec4092666562eb3fc32992ad478acb72550400c
ilanHascal1237 <ilanhascal1237@gmail.com>
2020-11-24 15:56:39 +0000

    changed name of client in delte.py

--- src/delete.py
+++ src/delete.py
@@ -5,8 +5,8 @@ from geometry_msgs.msg import *
 
 if __name__ == '__main__':
     print("Waiting for gazebo services...")
-    spawn_model_client = rospy.ServiceProxy('/gazebo/spawn_sdf_model', DeleteModel)
-    spawn_model_client(
+    delete_model_client = rospy.ServiceProxy('/gazebo/spawn_sdf_model', DeleteModel)
+    delete_model_client(
     model_name='TESTCYLINDER'
     )
     print("deleted")
