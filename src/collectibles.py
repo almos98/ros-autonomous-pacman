3d3455d53a99e270f1af1a68c04ecdcedace272d
Alexion Ramos <ratex98@gmail.com>
2020-12-01 03:53:04 +0000

    Parametised model functions and moved to shared file.

---
+++ src/collectibles.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import rospy, tf
+from gazebo_msgs.srv import DeleteModel, SpawnModel
+from geometry_msgs.msg import Pose
+
+spawn_model = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
+delete_model = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)
+
+MODEL_PATH = '/my_ros_data/catkin_ws/src/ros_autonomous_pacman/models/TESTCYLINDER/TESTCYLINDER.urdf'
+COLLECTIBLE_NAME = 'Collectible_%s_%s'
+
+# Takes floats x and y and spawns a model at that position.
+# x and y are in WORLD coordinates.
+def spawn_collectible(x, y):
+    pose = Pose()
+    pose.position.x = x
+    pose.position.y = y
+    resp = spawn_model(
+        model_name=COLLECTIBLE_NAME % (x, y),
+        model_xml=open(MODEL_PATH, 'r').read(),
+        initial_pose=pose,
+        reference_frame='world'
+    )
+
+    if not resp.success:
+        print("Error: %s" % resp.status_message)
+
+# Takes floats x and y and deletes the model at that position.
+# x and y are in WORLD coordinates.
+def delete_collectible(x, y):
+    resp = delete_model(
+        model_name=COLLECTIBLE_NAME % (x, y)
+    )
+
+    if not resp.success:
+        print("Error: %s" % resp.status_message)
