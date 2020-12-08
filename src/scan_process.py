91be65d5f7f8bb046c0877fc89d78eff15983fa8
ilanHascal1237 <ilanhascal1237@gmail.com>
2020-12-08 20:35:56 +0000

    modified the testCylinder URDF and scan_process.py

--- src/scan_process.py
+++ src/scan_process.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
-
+import math
 import rospy
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
-#from ros_autonomous_pacman.msg import ProcessedScan
+from ros_autonomous_pacman.msg import ProcessedScan
+from tf.transformations import euler_from_quaternion
 
+MIN_RANGE = .2
+# Class for processing Lidar and Odom data 
 class ScanProcess:
     def __init__(self):
         rospy.init_node("ScanProcessor")
@@ -13,22 +16,66 @@ class ScanProcess:
         self.odom_sub = rospy.Subscriber('pacman/odom', Odometry, self.odom_cb)
 
         self.scan_data = None
-        self.odom_data = None
-        
-        # self.pub = rospy.Publisher('pacman/scan/processed', ProcessedScan, queue_size=1)
+
+        self.yaw = None
+        self.pub = rospy.Publisher('pacman/scan/processed', ProcessedScan, queue_size=1)
+
+    ########################
+    # Subscriber Callbacks #
+    ########################
 
     def scan_cb(self, data):
         self.scan_data = data
 
     def odom_cb(self, data):
-        self.odom_data = data
+        # if data is None:
+        #     return
+        quat = data.pose.pose.orientation
+        #create a list of the orientation coordinates 
+        self.yaw = euler_from_quaternion([quat.x, quat.y, quat.z, quat.w])[2]  
+        
+
+    ########################
+    # Processing Function #
+    ########################
 
     def update(self):
+        if self.scan_data is None or self.yaw is None:
+            return
         # Process scan and odom data
         # Build message
         # Publish
-        pass
+        # rangesNorth = self.scan_data.ranges[:30] + self.scan_data.ranges[330:] 
+        # rangesEast = self.scan_data.ranges[54:124] 
+        # rangesSouth = self.scan_data.ranges[165:195] 
+        # rangesWest = self.scan_data.ranges[234:304] 
+ 
 
+        processed_data = ProcessedScan()
+
+        processed_data.facing = self.yaw
+        pi = MATH.pi
+        if self.yaw < (pi/4) and self.yaw > (7 * pi)/4:   
+            # facing north  
+            n_index = 0
+        elif self.yaw > (pi/4) and self.yaw < (3 * pi)/4:
+            n_index = 89
+        elif self.yaw > (3 * pi)/4 and self.yaw < (5 * pi)/4:
+            n_index = 180
+        elif self.yaw > (5 * pi)/4 and self.yaw < (7 * pi)/4:
+            n_index = 270
+
+        processed_data.north = min(self.subset(self.scan_data.ranges,n_index)) <= MIN_RANGE
+        processed_data.east = min(self.subset(self.scan_data.ranges, (n_index + 90) % 360)) <= MIN_RANGE
+        processed_data.south = min(self.subset(self.scan_data.ranges,(n_index + 180) % 360)) <= MIN_RANGE
+        processed_data.west = min(self.subset(self.scan_data.ranges,(n_index + 270) % 360)) <= MIN_RANGE
+
+        self.pub.publish(processed_data)
+
+    def subset(self,ranges, i, n=30):
+        return [ranges[x] for x in range(i-n,i+n) if x > self.scan_data.range_min]
+
+# MAIN FUNCTION 
 if __name__ == "__main__":
     process = ScanProcess()
 
